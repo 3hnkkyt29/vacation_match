710fc98bce1fcb7b74e06847ee515ace61fed1bf
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-06 08:59:19 +0000

    made models, controllers, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -7,6 +9,31 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
+
+  #ユーザー側
+    root to: 'public/homes#top'
+    get '/about' => 'public/homes#about'
+
+    scope module: :public do
+
+      get 'users/:id/confirm' => 'users#confirm', as: 'confirm'
+      patch 'users/:id/resign' => 'users#resign', as: 'resign'
+      get 'users/:id/favorites' => 'users#favorites', as: 'favorites'
+      resources :users, only: [:show, :edit, :update]
+
+      resources :posts, only: [:index, :new, :show, :edit, :create, :update, :destroy] do
+        resource :favorites, only: [:create, :destroy]
+        resources :comments, only: [:create, :destroy]
+      end
+
+    end
+
+  # 管理者側
+    namespace :admin do
+      get 'homes/top'
+      resources :users, only: [:show, :edit, :update]
+      resources :comments, only: [:index, :destroy]
+    end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20230606064756_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      
+      t.integer :user_id, null: false
+      t.integer :tag, null: false, default: 0
+      t.string :title, null: false
+      t.text :body, null: false
+      t.text :keyword, null: false
+
+      t.timestamps
+    end
+  end
+end
