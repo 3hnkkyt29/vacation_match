37fcd0962fd0723ea281f80c5c9f464a3f5ea888
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-07 04:33:52 +0000

    add category model

--- db/migrate/20230606064756_create_posts.rb
+++ db/migrate/20230606064756_create_posts.rb
@@ -1,8 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      
+
       t.integer :user_id, null: false
+      t.integer :category_id, null: false
       t.integer :tag, null: false, default: 0
       t.string :title, null: false
       t.text :body, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_06_064936) do
+ActiveRecord::Schema.define(version: 2023_06_07_042609) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2023_06_06_064936) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "category", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
@@ -69,6 +75,7 @@ ActiveRecord::Schema.define(version: 2023_06_06_064936) do
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
+    t.integer "category_id", null: false
     t.integer "tag", default: 0, null: false
     t.string "title", null: false
     t.text "body", null: false
