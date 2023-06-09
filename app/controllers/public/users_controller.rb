2b5bb823a25a8076b2175522b8341a48306f41d5
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-09 11:23:13 +0000

    fix guest_signin function *cannot be displayed edit_page*

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,4 +1,6 @@
 class Public::UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
@@ -27,5 +29,12 @@ class Public::UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :profile_image)
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guest"
+      redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
   end
 end
