5b325fe1311521f4b367ef36c8f75a18934611f6
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-10 05:58:14 +0000

    fix post/index, user/show *use partial template*

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -5,9 +5,8 @@ class Public::SearchesController < ApplicationController
     @content = params[:content].split(/[[:blank:]]+/)
     @records = Post.none
       @content.each_with_index do |content, i|
-      @records = Post.search_for(content) if i == 0
-      @records = @records.merge(@records.search_for(content))
-    # @records = Post.search_for(@content)
+        @records = Post.search_for(content) if i == 0
+        @records = @records.merge(@records.search_for(content))
       end
   end
 
