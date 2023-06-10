b89da94606d1c398847d956a1556b536584a574b
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-10 05:34:21 +0000

    fix keyword_search function *can search with many words*

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,8 +2,13 @@ class Public::SearchesController < ApplicationController
   # before_action :authenticate_user!
 
   def search
-    @content = params[:content]
-    @records = Post.search_for(@content)
+    @content = params[:content].split(/[[:blank:]]+/)
+    @records = Post.none
+      @content.each_with_index do |content, i|
+      @records = Post.search_for(content) if i == 0
+      @records = @records.merge(@records.search_for(content))
+    # @records = Post.search_for(@content)
+      end
   end
 
 end
