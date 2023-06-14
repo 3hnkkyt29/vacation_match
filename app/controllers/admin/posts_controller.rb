388f75fc336792bd965f64e32e6a1439307569f2
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-14 02:53:10 +0000

    fix post/index *use text truncation*

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,7 @@ class Admin::PostsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @posts = Post.includes(:user).where(users:{is_deleted: false}).page(params[:page]).per(30)
+    @posts = Post.includes(:user).where(users:{is_deleted: false}).page(params[:page]).per(48)
   end
 
   def show
