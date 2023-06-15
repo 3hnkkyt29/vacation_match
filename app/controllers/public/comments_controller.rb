b47c6bb929fe4d46daeb6df163875474c2e81880
3hnkkyt29 <129703843+3hnkkyt29@users.noreply.github.com>
2023-06-15 10:57:12 +0000

    fix comments into asynchronous communication

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,16 +2,16 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.save
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    comment = Comment.find(params[:id])
+    comment.destroy
+    @post = Post.find(params[:post_id])
   end
 
   private
